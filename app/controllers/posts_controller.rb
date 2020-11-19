1507eadc38a5a024f4842657ece78bdd9a80d580
dashdavid13 <davidamedina619@gmail.com>
2020-11-18 21:42:02 -0500

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params(:title))
@@ -27,6 +28,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  private
+
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
+  
   private
 
   def post_params(*args)
